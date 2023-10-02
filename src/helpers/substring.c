2f5b86e15e895bab47ca1297be472fc732b307e0
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2023-10-02 17:24:02 -0400

    Fixed issue with substring function adding characters

--- src/helpers/substring.c
+++ src/helpers/substring.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 char* substring(char* string, int start, int end) {
   int length = end - start;
 
   char* substr = malloc(length);
-
-  strncpy(substr, string+start, length);
+  sprintf(substr, "%.*s", length, string+start);
 
   return substr;
 }
