4917f167ceb2e44df3c4c6b50b2248faedc3d341
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2023-10-03 21:40:47 -0400

    Start on python transpiler for the raspberry pi pico

--- src/parser/transpilers/rpi_pico_python.c
+++ src/parser/transpilers/rpi_pico_python.c
@@ -1,18 +1,32 @@
 #include "../scanner.h"
 #include "../token_type.h"
+#include <string.h>
+
+char* type(Token* token) {
+  return "";
+}
 
 char* rpi_pico_python_transpiler(struct TokenArray* token_array) {
   Token* current_token;
 
+  char* transpiled_code;
+
   for ( int i = 0; i < token_array->size; i++ ) {
     current_token = token_array->array[i];
     
+    char* resulting_code;
+
     switch (current_token->type) {
-      case TYPE: break;
+      case TYPE:
+        i++;
+        resulting_code = type(token_array->array[i]);
+        break;
 
       default:
         break;
     }
+
+    strcat(transpiled_code, resulting_code);
   }
 
   return "";
