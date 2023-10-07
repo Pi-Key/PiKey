96fc14f39a119ad7e1a17b351cb1dd758e91d4f8
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2023-10-06 22:23:28 -0400

    FEAT: Started on if statements
    Small start on if statements still missing alot, just working on conditions for the moment, then execution and lastly it will be elifs and elses

--- src/parser/transpilers/rpi_pico_python.c
+++ src/parser/transpilers/rpi_pico_python.c
@@ -174,13 +174,73 @@ char* mode(Token* token) {
   return resulting_code;
 }
 
-char* rpi_pico_python_transpiler(struct TokenArray* token_array) {
-  Token* current_token;
+struct NumAndCode identifier(Token** token_array, int current) {
+  struct NumAndCode result;
 
-  char* transpiled_code = "import board\nfrom board import\nimport usb_hid\nfrom adafruit_hid.keyboard import Keyboard\nfrom adafruit_hid.keyboard_layout_us import KeyboardLayoutUS as KeyboardLayout\nfrom adafruit_hid.keycode import Keycode\n\nkbd=Keyboard(usb_hid.devices)\nlayout=KeyboardLayout(kbd)\n\ndef define_ducky_commands():\n    return {\n        'WINDOWS': Keycode.WINDOWS, 'GUI': Keycode.GUI,\n        'APP': Keycode.APPLICATION, 'MENU': Keycode.APPLICATION, 'SHIFT': Keycode.SHIFT,\n        'ALT': Keycode.ALT, 'CONTROL': Keycode.CONTROL, 'CTRL': Keycode.CONTROL,\n        'DOWNARROW': Keycode.DOWN_ARROW, 'DOWN': Keycode.DOWN_ARROW, 'LEFTARROW': Keycode.LEFT_ARROW,\n        'LEFT': Keycode.LEFT_ARROW, 'RIGHTARROW': Keycode.RIGHT_ARROW, 'RIGHT': Keycode.RIGHT_ARROW,\n        'UPARROW': Keycode.UP_ARROW, 'UP': Keycode.UP_ARROW, 'BREAK': Keycode.PAUSE,\n        'PAUSE': Keycode.PAUSE, 'CAPSLOCK': Keycode.CAPS_LOCK, 'DELETE': Keycode.DELETE,\n        'END': Keycode.END, 'ESC': Keycode.ESCAPE, 'ESCAPE': Keycode.ESCAPE, 'HOME': Keycode.HOME,\n        'INSERT': Keycode.INSERT, 'NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'PAGEUP': Keycode.PAGE_UP,\n        'PAGEDOWN': Keycode.PAGE_DOWN, 'PRINTSCREEN': Keycode.PRINT_SCREEN, 'ENTER': Keycode.ENTER,\n        'SCROLLLOCK': Keycode.SCROLL_LOCK, 'SPACE': Keycode.SPACE, 'TAB': Keycode.TAB,\n        'BACKSPACE': Keycode.BACKSPACE,\n        'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E,\n        'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J,\n        'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O,\n        'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T,\n        'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y,\n        'Z': Keycode.Z, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3,\n        'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7,\n        'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11,\n        'F12': Keycode.F12,\n    }\nduckyCommands = define_ducky_commands()\n\n";
+  Token next = *token_array[current + 1];
+  if (
+      next.type != EQUAL &&
+      next.type != PLUS_EQUAL &&
+      next.type != MINUS_EQUAL &&
+      next.type != LEFT_PARENTHESE
+    ) {
+    pk_error(formatter("[%d] Unexpected identifier.", next.line));
+    exit(1);
+  }
 
-  for ( int i = 0; i < token_array->size; i++ ) {
-    current_token = token_array->array[i];
+  result.resulting_code = formatter("%s ", token_array[current]);
+  
+  int i = current + 1;
+  while (token_array[i]->line == token_array[current]->line) {
+    if ( token_array[i]->type == TRUE ) {
+      strcat(result.resulting_code, " True");
+    } else if ( token_array[i]->type == FALSE ) {
+      strcat(result.resulting_code, " False");
+    } else {
+      strcat(result.resulting_code, formatter(" %s", token_array[i]->lexeme));
+    }
+    i++;
+  }
+
+  strcat(result.resulting_code, "\n");
+
+  result.i = i - 1;
+
+  return result;
+}
+
+struct NumAndCode transpile_if(Token** token_array, int current) {
+  struct NumAndCode result;
+
+  if ( token_array[current + 1] != LEFT_PARENTHESE ) {
+    pk_error(formatter("[%d] if statment expects parentheses around the condition.", token_array[current + 1]->line));
+    exit(1);
+  }
+
+  result.resulting_code = "if ";
+
+  int i = current + 2;
+  while ( token_array[i]->type != RIGHT_PARENTHESE && token_array[i+1]->type != LEFT_BRACE ) {
+    if ( token_array[i]->type == TRUE ) {
+      strcat(result.resulting_code, " True");
+    } else if ( token_array[i]->type == FALSE ) {
+      strcat(result.resulting_code, " False");
+    } else {
+      strcat(result.resulting_code, formatter(" %s", token_array[i]->lexeme));
+    }
+
+    i++;
+  }
+
+  strcat(result.resulting_code, "\n");
+
+  result.i = i - 1;
+
+  return result;
+}
+
+struct NumAndCode transpile_token(struct TokenArray* token_array, int i) {
+    Token* current_token = token_array->array[i];
     
     char* resulting_code;
 
@@ -238,10 +298,43 @@ char* rpi_pico_python_transpiler(struct TokenArray* token_array) {
         i++;
         resulting_code = mode(token_array->array[i]);
         break;
+      case IDENTIFIER:
+        {
+          struct NumAndCode new_i_and_code;
+          new_i_and_code = identifier(token_array->array, i);
+          resulting_code = new_i_and_code.resulting_code;
+          i = new_i_and_code.i;
+          break;
+        }
+      case IF:
+        {
+        struct NumAndCode new_i_and_code;
+        new_i_and_code = transpile_if(token_array->array, i);
+        resulting_code = new_i_and_code.resulting_code;
+        i = new_i_and_code.i;
+        break;
+      }
 
       default:
         break;
     }
+
+  struct NumAndCode final_i_and_code = {i, resulting_code};
+
+  return final_i_and_code;
+}
+
+char* rpi_pico_python_transpiler(struct TokenArray* token_array) {
+  Token* current_token;
+
+  char* transpiled_code = "import board\nfrom board import\nimport usb_hid\nfrom adafruit_hid.keyboard import Keyboard\nfrom adafruit_hid.keyboard_layout_us import KeyboardLayoutUS as KeyboardLayout\nfrom adafruit_hid.keycode import Keycode\n\nkbd=Keyboard(usb_hid.devices)\nlayout=KeyboardLayout(kbd)\n\ndef define_ducky_commands():\n    return {\n        'WINDOWS': Keycode.WINDOWS, 'GUI': Keycode.GUI,\n        'APP': Keycode.APPLICATION, 'MENU': Keycode.APPLICATION, 'SHIFT': Keycode.SHIFT,\n        'ALT': Keycode.ALT, 'CONTROL': Keycode.CONTROL, 'CTRL': Keycode.CONTROL,\n        'DOWNARROW': Keycode.DOWN_ARROW, 'DOWN': Keycode.DOWN_ARROW, 'LEFTARROW': Keycode.LEFT_ARROW,\n        'LEFT': Keycode.LEFT_ARROW, 'RIGHTARROW': Keycode.RIGHT_ARROW, 'RIGHT': Keycode.RIGHT_ARROW,\n        'UPARROW': Keycode.UP_ARROW, 'UP': Keycode.UP_ARROW, 'BREAK': Keycode.PAUSE,\n        'PAUSE': Keycode.PAUSE, 'CAPSLOCK': Keycode.CAPS_LOCK, 'DELETE': Keycode.DELETE,\n        'END': Keycode.END, 'ESC': Keycode.ESCAPE, 'ESCAPE': Keycode.ESCAPE, 'HOME': Keycode.HOME,\n        'INSERT': Keycode.INSERT, 'NUMLOCK': Keycode.KEYPAD_NUMLOCK, 'PAGEUP': Keycode.PAGE_UP,\n        'PAGEDOWN': Keycode.PAGE_DOWN, 'PRINTSCREEN': Keycode.PRINT_SCREEN, 'ENTER': Keycode.ENTER,\n        'SCROLLLOCK': Keycode.SCROLL_LOCK, 'SPACE': Keycode.SPACE, 'TAB': Keycode.TAB,\n        'BACKSPACE': Keycode.BACKSPACE,\n        'A': Keycode.A, 'B': Keycode.B, 'C': Keycode.C, 'D': Keycode.D, 'E': Keycode.E,\n        'F': Keycode.F, 'G': Keycode.G, 'H': Keycode.H, 'I': Keycode.I, 'J': Keycode.J,\n        'K': Keycode.K, 'L': Keycode.L, 'M': Keycode.M, 'N': Keycode.N, 'O': Keycode.O,\n        'P': Keycode.P, 'Q': Keycode.Q, 'R': Keycode.R, 'S': Keycode.S, 'T': Keycode.T,\n        'U': Keycode.U, 'V': Keycode.V, 'W': Keycode.W, 'X': Keycode.X, 'Y': Keycode.Y,\n        'Z': Keycode.Z, 'F1': Keycode.F1, 'F2': Keycode.F2, 'F3': Keycode.F3,\n        'F4': Keycode.F4, 'F5': Keycode.F5, 'F6': Keycode.F6, 'F7': Keycode.F7,\n        'F8': Keycode.F8, 'F9': Keycode.F9, 'F10': Keycode.F10, 'F11': Keycode.F11,\n        'F12': Keycode.F12,\n    }\nduckyCommands = define_ducky_commands()\n\n";
+
+  for ( int i = 0; i < token_array->size; i++ ) {
+
+    struct NumAndCode new_i_and_code = transpile_token(token_array, i);
+
+    i = new_i_and_code.i;
+    char* resulting_code = new_i_and_code.resulting_code;
 
     strcat(transpiled_code, resulting_code);
   }
